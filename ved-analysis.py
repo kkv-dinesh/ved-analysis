7ac3b9bd0557271804859ddb547635c0c5d9a2da
Dinesh K K V <132910073+kkv-dinesh@users.noreply.github.com>
2023-06-26 09:39:32 +0530

    Update ved-analysis.py

--- ved-analysis.py
+++ ved-analysis.py
@@ -1,72 +1,109 @@
+import os
 import pandas as pd
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, url_for
+from statsmodels.tsa.arima.model import ARIMA
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+app = Flask(__name__)
 
-        self.setWindowTitle("Excel Processor")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        self.result_table = QTableWidget(self)
-        self.result_table.setGeometry(QtCore.QRect(10, 10, 2000, 1000))
-        self.result_table.setColumnCount(6)
-        self.result_table.setHorizontalHeaderLabels(['Item Code', 'Item Description', 'Cost', 'Sales', 'Classification', 'Value'])
+@app.route('/upload', methods=['GET'  , 'POST'])
+def upload():
+    # Check if a file was uploaded
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
 
-        self.process_excel()
+    file = request.files['file']
+    file_path = os.path.join('uploads', file.filename)
+    file.save(file_path)
 
-    def process_excel(self):
-        # Define the Excel file path
-        excel_file = r"D:\sem-4\bpo\Excel files\VED.xlsx"
+    # Perform VED analysis
+    ved_results, chart_path = perform_ved_analysis(file_path)
 
-        # Read the inventory data from the Excel file
-        ved = pd.read_excel(excel_file)
+    # Render the results template with VED analysis results and chart path
+    return render_template('results.html', ved_results=ved_results, chart_path=chart_path)
 
-        # Calculate the demand based on the desired columns in your dataset
-        ved['demand'] = ved['sales'] + ved['manufacturing_time'] + ved['processing_time'] + ved['transit_time'] + ved['lead_time']
+def perform_ved_analysis(file_path):
+    # Step 1: Load your data into a pandas DataFrame
+    data = pd.read_csv(file_path)
+    # Step 2: Preprocess the data
+    data = data.fillna(method='ffill')
 
-        # Find the column name for demand
-        demand_column = 'demand'
+    # Step 3: Group the data by 'Sub_Category' and aggregate the 'no_of_ratings' column
+    demand_data = data.groupby('Sub_Category')['no_of_ratings'].sum().reset_index()
 
-        # Check if demand column exists
-        if demand_column not in ved.columns:
-            self.result_table.setRowCount(1)
-            self.result_table.setItem(0, 0, QTableWidgetItem("Demand column not found in the Excel file."))
-            return
+    # Step 4: Filter out rows with out-of-range integer values in 'no_of_ratings'
+    demand_data['no_of_ratings'] = pd.to_numeric(demand_data['no_of_ratings'], errors='coerce')
+    demand_data = demand_data.dropna(subset=['no_of_ratings'])
+    demand_data['no_of_ratings'] = demand_data['no_of_ratings'].astype(int)
 
-        # Calculate the value of each item and sort the items by value
-        ved['value'] = ved[demand_column] * ved['sales']
-        ved = ved.sort_values(by='value', ascending=False)
+    # Step 5: Fit an ARIMA model and forecast for each Sub_Category
+    forecasts = {}
 
-        # Determine the VED classification for each item
-        total_demand = ved[demand_column].sum()
-        ved['cumulative_demand'] = ved[demand_column].cumsum() / total_demand
-        ved.loc[ved['cumulative_demand'] <= 0.8, 'classification'] = 'Vital'
-        ved.loc[(ved['cumulative_demand'] > 0.8) & (ved['cumulative_demand'] <= 0.95), 'classification'] = 'Essential'
-        ved.loc[ved['cumulative_demand'] > 0.95, 'classification'] = 'Desirable'
+    for sub_category, sub_data in data.groupby('Sub_Category'):
+        sub_data = sub_data.copy()
+        sub_data['no_of_ratings'] = pd.to_numeric(sub_data['no_of_ratings'], errors='coerce')
+        sub_data = sub_data.dropna(subset=['no_of_ratings'])
+        sub_data['no_of_ratings'] = sub_data['no_of_ratings'].astype(int)
+        sub_data = sub_data.loc[sub_data['no_of_ratings'] >= 0]  # Drop rows with negative ratings
 
-        # Display the results in the table
-        self.result_table.setRowCount(len(ved))
-        for row in range(len(ved)):
-            item_code = QTableWidgetItem(str(ved.iloc[row]['ITEM CODE']))
-            item_description = QTableWidgetItem(str(ved.iloc[row]['ITEM DESCRIPTION']))
-            cost = QTableWidgetItem(str(ved.iloc[row]['cost']))
-            sales = QTableWidgetItem(str(ved.iloc[row]['sales']))
-            classification = QTableWidgetItem(str(ved.iloc[row]['classification']))
-            value = QTableWidgetItem('{:.2f}'.format(ved.iloc[row]['value']))
+        if len(sub_data) > 1:
+            sub_data = sub_data.reset_index(drop=True)  # Reset the index
 
-            self.result_table.setItem(row, 0, item_code)
-            self.result_table.setItem(row, 1, item_description)
-            self.result_table.setItem(row, 2, cost)
-            self.result_table.setItem(row, 3, sales)
-            self.result_table.setItem(row, 4, classification)
-            self.result_table.setItem(row, 5, value)
+            model = ARIMA(sub_data['no_of_ratings'], order=(1, 0, 1))
+            model_fit = model.fit()
+            forecast = model_fit.forecast(steps=1)  # Forecast only one step ahead
 
-if __name__ == "__main__":
-    import sys
+            if forecast is not None and len(forecast) > 0:
+                if isinstance(forecast, np.ndarray):
+                    forecasts[sub_category] = forecast[0]
+                elif isinstance(forecast, pd.Series):
+                    forecasts[sub_category] = forecast.iloc[0]
+                else:
+                    forecasts[sub_category] = np.nan
+            else:
+                forecasts[sub_category] = np.nan
+        else:
+            forecasts[sub_category] = np.nan
 
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.resize(620, 460)
-    main_window.show()
-    sys.exit(app.exec())
+    # Perform VED analysis
+    df_demand_forecast = pd.DataFrame({
+        'Sub_Category': list(forecasts.keys()),
+        'Forecasted_Demand': list(forecasts.values())
+    })
+
+    df_demand_forecast['Cumulative_Demand'] = df_demand_forecast['Forecasted_Demand'].cumsum()
+    total_demand = df_demand_forecast['Forecasted_Demand'].sum()
+    df_demand_forecast['Percentage_Demand'] = df_demand_forecast['Forecasted_Demand'] / total_demand * 100
+    df_demand_forecast['Cumulative_Percentage'] = df_demand_forecast['Percentage_Demand'].cumsum()
+
+    df_demand_forecast['VED_Category'] = pd.cut(df_demand_forecast['Cumulative_Percentage'],
+                                                bins=[0, 70, 90, 100],
+                                                labels=['Vital', 'Essential', 'Desirable'])
+
+    ved_results = df_demand_forecast.to_dict(orient='records')
+
+    # Count the number of Vital, Essential, and Desirable items
+    ved_counts = df_demand_forecast['VED_Category'].value_counts()
+
+    # Generate the pie chart
+    ved_labels = ['Vital', 'Essential', 'Desirable']
+    ved_values = ved_counts.values.tolist()
+
+    fig, ax = plt.subplots()
+    ax.pie(ved_values, labels=ved_labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+
+    chart_path = os.path.join('static', 'ved_chart3.png')
+
+    plt.savefig(chart_path)
+    plt.close()
+
+    return ved_results, chart_path
+
+if __name__ == '__main__':
+    app.run(debug=True)
